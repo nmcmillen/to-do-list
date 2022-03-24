df4a1e9adfb9a8c48756581ee2f98f64a0f4bc9f
Nick McMillen <nick.mcmillen@student.awesomeinc.org>
2022-03-24 00:47:47 +0000

    local storage fully working

--- src/App.js
+++ src/App.js
@@ -4,29 +4,25 @@ import { useState, useEffect } from "react";
 import CreateTask from "./CreateTask";
 
 function App() {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      note: "Give dog a bath",
-      complete: true,
-    },
-    {
-      id: 2,
-      note: "Do laundry",
-      complete: true,
-    },
-    {
-      id: 3,
-      note: "Vacuum floor",
-      complete: false,
-    },
-  ]);
+  const [tasks, setTasks] = useState([]);
+
   // console logging tasks array for status updates
   console.log(tasks);
 
+  // only run when first rendered to pull local storage if it exists
+  // pulls from the stringified data so have to parse to put in array again
+  // using "keytasklist" as the KEY argument which I understand as key or reference to the data to fetch
   useEffect(() => {
-    console.log("setting state test");
+    if (localStorage.getItem("keytasklist")) {
+      setTasks(JSON.parse(localStorage.getItem("keytasklist")))
+    }
   }, []);
+
+  // local storage setup for when "tasks" changes it will save to local storage
+  // can't store "tasks" array so have to stringify since local storage only accepts strings of text
+  useEffect(() => {
+    localStorage.setItem("keytasklist", JSON.stringify(tasks))
+  }, [tasks]);
 
   return (
     <div className="App">
@@ -35,6 +31,7 @@ function App() {
       <div>
         {tasks.map((task) => (
           <Task
+            tasks={tasks}
             setTasks={setTasks}
             id={task.id}
             key={task.id}
@@ -75,17 +72,59 @@ function App() {
 
 // Creates the actual div/area and buttons for the actions on each task/note
 function Task(props) {
+  // function deletes task
   function handleDelete() {
-    props.setTasks((state) => state.filter((task) => task.id != props.id));
+    // Changes (setTasks) state to create new array. Filter runs on each item and if the task.id is not equal
+    // to the clicked ID, it will return true and will go to new array. If value false it will be skipped
+    // and deleted 
+    props.setTasks((state) => state.filter(task => task.id !== props.id));
+  }
+
+  // function updates the task status to complete true or false
+  function handleComplete(id) {
+    const updatedTasks = props.tasks.map((task) => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        // use object spread to make a new object
+        // whose `completed` prop has been inverted
+        return { ...task, complete: !task.complete };
+      }
+      return task;
+    });
+    props.setTasks(updatedTasks);
   }
 
   return (
     <div id="created-task">
       {props.note}
-      {/* <button onClick={handleComplete}>Complete</button> */}
+      <input
+        id={props.id}
+        type="checkbox"
+        onChange={() => handleComplete(props.id)}
+        defaultChecked={props.complete}
+      ></input>
       <button onClick={handleDelete}>Delete</button>
+      {/* create a delete all button function here??? */}
     </div>
   );
 }
 
 export default App;
+
+
+
+    // {
+    //   id: 1,
+    //   note: "Give dog a bath",
+    //   complete: true,
+    // },
+    // {
+    //   id: 2,
+    //   note: "Do laundry",
+    //   complete: true,
+    // },
+    // {
+    //   id: 3,
+    //   note: "Vacuum floor",
+    //   complete: false,
+    // },
